c47f54b238158de959a100fd10bccf5e90817e5e
KayneWest <mkrzus3@gmail.com>
2014-10-15 14:28:47 -0500

    new stuff
    new stuff

--- templates/agency/js/auto_reply.js
+++ templates/agency/js/auto_reply.js
@@ -1,31 +1,63 @@
+
 $(function() {
     $('#subscribeForm').bind('submit', function() {
-        $.ajax({
-            url: "auto_reply.php",
-            type: "POST",
-            data: {"mceEMAIL" : mceEMAIL}, // here build JSON object with your form data
-            dataType: "json",
-            contentType: "application/json",
-            success: function() {
-                // this is returned value from your PHP script
-                //your PHP script needs to send back JSON headers + JSON object, not new HTML document!
-                // update your "message" element in the modal
+        $("input").jqBootstrapValidation({
+            preventSubmit: true,
+            submitError: function($form, event, errors) {
+                // additional error messages or events
+            },
+            submitSuccess: function($form, event) {
+                event.preventDefault(); // prevent default submit behaviour
+                // get values from FORM
+                var email = $("input#mceEMAIL").val();
+                $.ajax({
+                    url: "././mail/auto_reply.php",
+                    type: "POST",
+                    data: {
+                        mceEMAIL: mceEMAIL,
+                    },
+                    cache: false,
+                    success: function() {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
 
-                    $('#success_').html("<div class='alert alert-success'>");
-                    $('#success_ > .alert-success').html("<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success_ > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success_ > .alert-success')
-                        .append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                    error: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='input' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + mceEMAIL + ", it seems that my mail server is not responding. Please try again later!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                })
+            },
+            filter: function() {
+                return $(this).is(":visible");
+            },
+        });
 
-                    //clear all fields
-                    $('#subscribeForm').txt();
-
-                $("#subscribeForm").text(msg);
-            }
+        $("a[data-toggle=\"tab\"]").click(function(e) {
+            e.preventDefault();
+            $(this).tab("show");
         });
     });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
 
 
-success_
+
